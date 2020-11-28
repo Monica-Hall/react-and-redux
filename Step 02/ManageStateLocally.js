b32de3ed7acc92bc38925a70e94a263d3a744370
Monica-Hall <monicakes@Monicas-MacBook-Air.local>
2020-11-27 17:16:44 -0700

    managed state locally, completed challenge

---
+++ Step 02/ManageStateLocally.js
@@ -0,0 +1,44 @@
+class DisplayMessages extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        input: '',
+        messages: []
+      }
+      this.handleChange = this.handleChange.bind(this);
+      this.submitMessage = this.submitMessage.bind(this); 
+    }
+  
+    // Add handleChange() and submitMessage() methods here
+    handleChange(e) {
+      this.setState({
+        input: e.target.value, 
+        messages: this.state.messages
+      })
+    }
+  
+    submitMessage() {
+      this.setState({
+        input: "", 
+        messages: [...this.state.messages, this.state.input]
+      })
+    }
+  
+    render() {
+      return (
+        <div>
+          <h2>Type in a new Message:</h2>
+          { /* Render an input, button, and ul below this line */ }
+          <input onChange={this.handleChange} value={this.state.input}/> 
+          <button onClick={this.submitMessage}>Submit</button>
+          <ul> 
+          {this.state.messages.map((x, i) => {
+            return <li key={i}>{x}</li>
+          })}
+          </ul>
+  
+          { /* Change code above this line */ }
+        </div>
+      );
+    }
+  };
